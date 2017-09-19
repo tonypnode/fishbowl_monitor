96b4746d1a91e083b70b530cac6e4b507f2ecc47
sgtp <3esk6j35#ESK^J#%>
2017-09-18 20:46:15 -0400

    YEEESSSS

--- fish_bowl_monitor/admin.py
+++ fish_bowl_monitor/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from .models import TankTemp
 
 # Register your models here.
+admin.site.register(TankTemp)
--- fish_bowl_monitor/views.py
+++ fish_bowl_monitor/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render
-
+from fish_bowl_monitor.models import TankTemp
 # Create your views here.
 
 
 def fish_home(request):
-    return render(request, 'home.html')
+    tank_temp = TankTemp.objects.all().first()
+    return render(request, 'home.html', {'tank_temp': tank_temp.temperature_data})
