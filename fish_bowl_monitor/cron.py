7dfaad2611b20fd3812f005d46dc1ead09c8a76f
sgtp <3esk6j35#ESK^J#%>
2017-09-29 19:36:24 -0400

    update cron to keep 8k records

--- fish_bowl_monitor/cron.py
+++ fish_bowl_monitor/cron.py
@@ -6,8 +6,8 @@ from fish_bowl_monitor.temperature_probe import TempData
 def temperature_cron():
     tank_temp = TempData()
     read_out = tank_temp.read_temp()
-    print(read_out)
     TankTemp.objects.create(temperature_data=read_out)
-
-
-
+    total = TankTemp.objects.all().count()
+    while total > 8000:
+        yo = {rm.delete() for rm in TankTemp.objects.earliest()}
+        print('deleted some shit')
--- fish_bowl_monitor/views.py
+++ fish_bowl_monitor/views.py
@@ -1,11 +1,10 @@
 from django.shortcuts import render
 from fish_bowl_monitor.models import TankTemp
-from fish_bowl_monitor.temperature_probe import TempData
+# from fish_bowl_monitor.temperature_probe import TempData
 
 # Create your views here.
 
 
 def fish_home(request):
-
     check_db = TankTemp.objects.latest('date_time_stamp')
     return render(request, 'home.html', {'tank_temp': check_db.temperature_data})
