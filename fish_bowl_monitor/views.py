df97ff98249bac20b01df6690e29e415db66549f
sgtp <3esk6j35#ESK^J#%>
2017-09-29 21:22:28 -0400

    add earliest temp reading and date to page

--- fish_bowl_monitor/views.py
+++ fish_bowl_monitor/views.py
@@ -1,10 +1,13 @@
 from django.shortcuts import render
 from fish_bowl_monitor.models import TankTemp
-# from fish_bowl_monitor.temperature_probe import TempData
-
-# Create your views here.
 
 
 def fish_home(request):
     check_db = TankTemp.objects.latest('date_time_stamp')
-    return render(request, 'home.html', {'tank_temp': check_db.temperature_data})
+    oldest = TankTemp.objects.earliest('date_time_stamp')
+    return render(request, 'home.html', {
+        'current_tank_temp': check_db.temperature_data,
+        'earliest_tank_temp': oldest.temperature_data,
+        'earliest_date': oldest.date_time_stamp,
+        }
+    )
